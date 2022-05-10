37d2818246fcf91fc8b8e39331fcbb5c8dd35717
ysari97 <y.sari@student.tudelft.nl>
2022-05-11 00:25:02 +0200

    Clean-up part2

--- Nile_EMODPS_framework/Experimentation/model_wrapper.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-def model_wrapper(**kwargs):
-    input = [kwargs['v' + str(i)] for i in range(len(kwargs))]
-    Hydropower, Environment, Irrigation = tuple(kwargs["model"].evaluate(np.array(input)))
-    return Hydropower, Environment, Irrigation
