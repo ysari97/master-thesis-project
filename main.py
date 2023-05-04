3f535d3788b221157cde70a0dd54e17135fe59ad
MeronZnabei <meronznabei@hotmail.com>
2023-05-04 21:56:56 +0200

    File structure changes

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Main document to run baseline optimization, experiments and tests.
+"""
+
+from experimentation import baseline_optimization
+
+
+if __name__ == '__main__':
+    baseline_optimization.run()
