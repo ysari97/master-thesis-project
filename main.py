b1dad2c11566561a45f2a569f678a3560d1a7818
MeronZnabei <meronznabei@hotmail.com>
2023-07-24 23:35:25 +0200

    Update output structure for experiments

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from experimentation import baseline_optimization
 
 if __name__ == '__main__':
     # Access the environment variables for input parameters
-    nfe = int(os.environ.get("NFE", 2))
-    epsilon_list = [float(epsilon) for epsilon in os.environ.get("EPSILON_LIST", "0.01 0.001 0.001 0.01 0.001 0.01").split()]
-    convergence_freq = int(os.environ.get("CONVERGENCE_FREQ", 1))
-    experiment = os.environ.get("EXPERIMENT", "my_experiment")
+    nfe = int(os.environ.get("NFE"))
+    epsilon_list = [float(epsilon) for epsilon in os.environ.get("EPSILON_LIST").split()]
+    convergence_freq = int(os.environ.get("CONVERGENCE_FREQ"))
+    description = os.environ.get("DESCRIPTION")
 
     # call the baseline optimization function 'run()' with the provided experiment input
-    baseline_optimization.run(nfe, epsilon_list, convergence_freq, experiment)
+    baseline_optimization.run(nfe, epsilon_list, convergence_freq, description)
