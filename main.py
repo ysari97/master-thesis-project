ea282bdaba1b0bb9532cb006d1e37a90aabee8f5
MeronZnabei <meronznabei@hotmail.com>
2023-07-30 16:15:45 +0200

    first results settings

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
 
     DEBUG = True
     if DEBUG:
-        nfe = 2
-        epsilon_list = [1e-1, 1e-2, 1e-2, 1e-1, 1e-2, 1e-1, 1e-1]
-        convergence_freq = 1
-        description = "debug"
+        nfe = 5000
+        epsilon_list = [1e-2, 1e-4, 1e-4, 1e-2, 1e-4, 1e-2, 1e-2]
+        convergence_freq = 250
+        description = "uwf"
         principle = "uwf"           # possible principles uwf, swf, pwf, gini
     else:
         # Access the environment variables for input parameters
